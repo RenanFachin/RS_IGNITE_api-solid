59b922adfcaa93c0f152b65eb68230a7fdea9811
Renan <renan.fachin@hotmail.com>
2023-04-25 14:30:48 -0300

    Configura o ESLint

--- src/env/index.ts
+++ src/env/index.ts
@@ -6,16 +6,15 @@ import { z } from 'zod'
 // validar as variáveis de ambiente
 const envSchema = z.object({
   NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
-  PORT: z.coerce.number().default(3333) // coerce faz a conversão do dado
+  PORT: z.coerce.number().default(3333), // coerce faz a conversão do dado
 })
 
 // Validando o que está dentro do .env com o objeto de validação criado
 const _env = envSchema.safeParse(process.env)
 
-if(_env.success === false){
+if (_env.success === false) {
   // o método format() vai apenas formatar o que "apitou" o erro
   console.error('Invalid environment variables', _env.error.format())
-
 
   // este throw new error é para garantir que nada mais será executado. A aplicação nem vai subir caso as variáveis de ambiente apresentem algum erro
   throw new Error('Invalid environment variables')
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
-import { app } from "./app";
-import { env } from "./env";
+import { app } from './app'
+import { env } from './env'
 
-app.listen({
-  host: '0.0.0.0', // faz ser acessível pelo front-end
-  port: env.PORT,
-}).then(
-  () => {
+app
+  .listen({
+    host: '0.0.0.0', // faz ser acessível pelo front-end
+    port: env.PORT,
+  })
+  .then(() => {
     console.log('HTTP Server Running!')
-  }
-)
+  })
