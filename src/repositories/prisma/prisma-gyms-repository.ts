273fad4c1753756716b8cb41c7a2d734b3f0d428
Renan <renan.fachin@hotmail.com>
2023-04-29 19:55:20 -0300

    Implementa repositório do prisma para gyms

---
+++ src/repositories/prisma/prisma-gyms-repository.ts
@@ -0,0 +1,49 @@
+import { Gym, Prisma } from '@prisma/client'
+import { FindManyNearbyParams, GymsRepository } from '../gyms-repository'
+import { prisma } from '@/lib/prisma'
+
+export class PrismaGymsRepository implements GymsRepository {
+  async findById(id: string) {
+    const gym = await prisma.gym.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    return gym
+  }
+
+  async create(data: Prisma.GymCreateInput) {
+    const gym = await prisma.gym.create({
+      data,
+    })
+
+    return gym
+  }
+
+  async searchMany(query: string, page: number) {
+    const gyms = await prisma.gym.findMany({
+      where: {
+        // Passando um objeto para buscar um título que contenha
+        title: {
+          contains: query,
+        },
+      },
+      take: 20,
+      skip: (page - 1) * 20,
+    })
+
+    return gyms
+  }
+
+  async findManyNearby({ latitude, longitude }: FindManyNearbyParams) {
+    // fazendo a busca por próximos utilizando querys do sql
+    // Quando utilizamos a queryRaw o typescript não consegue inferir a tipagem
+    const gyms = await prisma.$queryRaw<Gym[]>`
+    SELECT * from gyms
+    WHERE ( 6371 * acos( cos( radians(${latitude}) ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(${longitude}) ) + sin( radians(${latitude}) ) * sin( radians( latitude ) ) ) ) <= 10
+    `
+
+    return gyms
+  }
+}
