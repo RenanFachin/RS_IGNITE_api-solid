d4c61c8e114093300f602f5fd66eac8a89fac8fb
Renan <renan.fachin@hotmail.com>
2023-04-30 18:42:36 -0300

    Cria middleware para validar o JWT

---
+++ src/http/middlewares/verify-jwt.ts
@@ -0,0 +1,14 @@
+import { FastifyRequest, FastifyReply } from 'fastify'
+
+export async function verifyJWT(
+  request: FastifyRequest,
+  response: FastifyReply,
+) {
+  try {
+    await request.jwtVerify()
+  } catch (err) {
+    return response.status(401).send({
+      message: 'Unauthorized',
+    })
+  }
+}
