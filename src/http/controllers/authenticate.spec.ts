64f8ecea6328c991f7029864dc6ce4c4eeeb2c10
Renan <renan.fachin@hotmail.com>
2023-04-30 23:10:39 -0300

    Cria teste e2e de autenticação

--- src/http/controllers/authenticate.spec.ts
+++ src/http/controllers/authenticate.spec.ts
@@ -1,3 +1,33 @@
-import { test } from 'vitest'
+import request from 'supertest'
+import { it, describe, expect, beforeAll, afterAll } from 'vitest'
+import { app } from '@/app'
 
-test('ok', () => {})
+describe('Authenticate (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to authenticate', async () => {
+    // Passo 1: Criar usuário
+    await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'john@example.com',
+      password: '1234567',
+    })
+
+    // Passo 2: Autenticação
+    const response = await request(app.server).post('/sessions').send({
+      email: 'john@example.com',
+      password: '1234567',
+    })
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body).toEqual({
+      token: expect.any(String),
+    })
+  })
+})
